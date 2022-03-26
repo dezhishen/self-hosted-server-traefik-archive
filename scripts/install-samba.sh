cf46bb68919b3e73ddd6914ae1aa6b2d53d9496f
dezhiShen <1179551960@qq.com>
2022-03-26 19:00:57 +0800

    mod:修改samba配置方式

--- scripts/install-samba.sh
+++ scripts/install-samba.sh
@@ -35,15 +35,16 @@ docker run -d --restart=always --name=samba \
 -p 139:139 -p 445:445 \
 -e TZ="Asia/Shanghai" \
 -e LANG="zh_CN.UTF-8" \
--e SHARE="www;/mount/;yes;no;no;all;none" \
--e USER="$SAMBA_USER_NAME;$SAMBA_USER_PASSWORD" \
+-v $base_data_dir/public:/mount \
 -e USERID=$(id -u) \
 -e GROUPID=$(id -g) \
--v $base_data_dir/public:/mount \
-dperson/samba
+dperson/samba \
+-s "shared;/mount/;yes;no;no;all;none;" \
+-u "$SAMBA_USER_NAME;$SAMBA_USER_PASSWORD" \
 
 echo "samba启动完成"
 echo "请调整防火墙规则，允许内网访问 139/445 端口"
 # 调整防火墙规则，允许内网访问 139/445 端口
 echo "修改下方命令的192.168.31.0/24段，替换为你的内网IP段"
 echo "如 sudo ufw from 192.168.31.0/24 to any port 139,445 proto tcp"
+echo 'user \\$ip\shared to mount your share'
