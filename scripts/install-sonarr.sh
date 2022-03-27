ef2fa014d8b1a09d4b91421e22afd556d1c5b494
dezhiShen <1179551960@qq.com>
2022-03-27 10:51:15 +0800

    mod:增加镜像版本选择

--- scripts/install-sonarr.sh
+++ scripts/install-sonarr.sh
@@ -6,6 +6,27 @@ docker_network_name=$3
 
 `dirname $0`/stop-container.sh sonarr
 
+echo "请选择服务架构: "
+echo "1. x86-64	"
+echo "2. arm64"
+echo "3. armhf"
+read -p "请输入序号: " num
+case $num in
+    1 )
+        arch="amd64"
+        ;;
+    2 )
+        arch="arm64v8"
+        ;;
+    3 )
+        arch="arm32v7"
+        ;;
+    * )
+        echo "输入错误,即将退出安装..."
+        exit 0
+        ;;
+esac
+
 docker run -d --name=sonarr \
 --restart=always \
 -m 50M --memory-swap=100M \
@@ -23,7 +44,7 @@ docker run -d --name=sonarr \
 --label "traefik.http.routers.sonarr.tls.domains[0].main=sonarr.$domain" \
 --label "traefik.http.services.sonarr.loadbalancer.server.port=8989" \
 --label "traefik.enable=true" \
-lscr.io/linuxserver/sonarr
+lscr.io/linuxserver/sonarr:$arch
 
 echo "启动sonarr容器"
 echo "访问 https://sonarr.$domain "
