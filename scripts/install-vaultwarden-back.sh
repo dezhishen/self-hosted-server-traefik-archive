5dd61f38da05a86c6f0f8c329186127c259da343
dezhiShen <1179551960@qq.com>
2022-03-29 20:37:27 +0800

    fix:错误的名称

--- scripts/install-vaultwarden-back.sh
+++ scripts/install-vaultwarden-back.sh
@@ -4,13 +4,13 @@ domain=$1
 base_data_dir=$2
 docker_network_name=$3
 # echo "拉取镜像..."
-# docker pull ttionya/vaultwarden-backup
+# docker pull ttionya/vaultwarden-back
 
-`dirname $0`/create-dir.sh $base_data_dir/vaultwarden-backup
-`dirname $0`/create-dir.sh $base_data_dir/vaultwarden-backup/config
+`dirname $0`/create-dir.sh $base_data_dir/vaultwarden-back
+`dirname $0`/create-dir.sh $base_data_dir/vaultwarden-back/config
 
 echo "当前rclone配置如下: "
-docker run --rm -it -v $base_data_dir/vaultwarden-backup/config:/config ttionya/vaultwarden-backup:latest rclone config show
+docker run --rm -it -v $base_data_dir/vaultwarden-back/config:/config ttionya/vaultwarden-back:latest rclone config show
 
 echo ""
 
@@ -18,8 +18,8 @@ read -p "是否需要修改配置文件？(y/n)" yn
 case $yn in
     [Yy]* )
         docker run --rm -it \
-        -v $base_data_dir/vaultwarden-backup/config:/config \
-        ttionya/vaultwarden-backup:latest \
+        -v $base_data_dir/vaultwarden-back/config:/config \
+        ttionya/vaultwarden-back:latest \
         rclone config
         ;;
 esac
@@ -50,17 +50,17 @@ echo "备份压缩文件的密码为: $BACKUP_ZIP_PASSWORD"
 
 echo "备份到的rclone远程目录为: $MY_BACKUP_RCLONE_REMOTE"
 
-`dirname $0`/stop-container.sh vaultwarden-backup
+`dirname $0`/stop-container.sh vaultwarden-back
 docker run -d \
 --restart=always \
 -m 50M \
---name vaultwarden-backup \
--v $base_data_dir/vaultwarden-backup/config:/config \
+--name vaultwarden-back \
+-v $base_data_dir/vaultwarden-back/config:/config \
 -v $base_data_dir/vaultwarden/data:/vaultwarden/data \
 -e ZIP_PASSWORD=$BACKUP_ZIP_PASSWORD \
 -e DATA_DIR=/vaultwarden/data \
 -e RCLONE_REMOTE_NAME="$MY_BACKUP_RCLONE_REMOTE" \
 -e TZ=Asia/Shanghai \
 -e LANG=zh_CN.UTF-8 \
-ttionya/vaultwarden-backup:latest
+ttionya/vaultwarden-back:latest
 echo "备份服务启动完成"
