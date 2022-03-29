7a1a7a056e8c456f587773dcb7293275a55fb5a7
dezhiShen <1179551960@qq.com>
2022-03-29 20:32:20 +0800

    mod:修改行尾

--- scripts/stop-container.sh
+++ scripts/stop-container.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
 docker_container_name=$1
+
 echo "正在停止容器: $docker_container_name"
+
 docker ps -a -q --filter "name=$docker_container_name" | grep -q . && docker rm -fv $docker_container_name
