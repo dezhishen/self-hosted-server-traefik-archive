7660ebc680e6a1d8aa9713c2657a5065640133d6
shendezhi <shendz@cecinvestment.com>
2022-03-29 15:19:53 +0800

    fix:启动dockerproxy避免安全问题

--- scripts/install-traefik.sh
+++ scripts/install-traefik.sh
@@ -13,6 +13,26 @@ if [ -z "$acme_email" ]; then
         `dirname $0`/set-args.sh acme_email $acme_email
     fi
 fi
+ 
+echo "停止dockerproxy"
+
+docker_container_name=dockerproxy
+
+docker ps -a -q --filter "name=$docker_container_name" | grep -q . && docker rm -fv $docker_container_name
+
+docker run \
+    --privileged \
+    -m 32M --memory-swap 64M \
+    -e CONTAINERS=1 \
+    -e NETWORKS=1 \
+    -d --restart=always \
+    --network=$docker_network_name --network-alias=dockerproxy \
+    --name dockerproxy \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    tecnativa/docker-socket-proxy
+
+
+docker_container_name=traefik
 
 echo "停止之前的traefik容器"
 docker_container_name=traefik
@@ -20,7 +40,7 @@ docker ps -a -q --filter "name=$docker_container_name" | grep -q . && docker rm 
 
 echo "启动traefik容器"
 docker run --name=traefik \
---restart=always -d -m 50M \
+--restart=always -d -m 64M --memory-swap 128M \
 -e TZ="Asia/Shanghai" \
 -e LANG="zh_CN.UTF-8" \
 -p 80:80 -p 443:443 \
@@ -28,6 +48,7 @@ docker run --name=traefik \
 -v /var/run/docker.sock:/var/run/docker.sock \
 -v $base_data_dir/traefik/acme:/acme traefik \
 --providers.docker=true \
+--providers.docker.endpoint=tcp://dockerproxy:2375 \
 --providers.docker.exposedbydefault=false \
 --entrypoints.web.address=":80" \
 --entrypoints.web.http.redirections.entryPoint.to=websecure \
