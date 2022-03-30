d8d63e7e3a174e9ce7bdb6310f1985fbae35ba06
dezhiShen <1179551960@qq.com>
2022-03-30 21:47:58 +0800

    feat:增加v2raya

---
+++ scripts/install-v2raya.sh
@@ -0,0 +1,23 @@
+# !/bin/bash
+domain=$1
+base_data_dir=$2
+docker_network_name=$3
+
+docker_container_name=v2raya
+docker ps -a -q --filter "name=$docker_container_name" | grep -q . && docker rm -fv $docker_container_name
+
+docker run -d \
+    --name v2raya \
+    --restart=always \
+    -e LANG=C.UTF-8 \
+    -e TZ=Asia/Shanghai \
+    --network=$docker_network_name \
+    --network-alias=v2raya \
+    --label 'traefik.http.routers.v2raya.rule=Host(`v2raya'.$domain'`)' \
+    --label "traefik.http.routers.v2raya.tls=true" \
+    --label "traefik.http.routers.v2raya.tls.certresolver=traefik" \
+    --label "traefik.http.routers.v2raya.tls.domains[0].main=v2raya.$domain" \
+    --label "traefik.http.services.v2raya.loadbalancer.server.port=8989" \
+    --label "traefik.enable=true" \
+    -v $base_data_dir/v2raya:/etc/v2raya \
+  mzz2017/v2raya
