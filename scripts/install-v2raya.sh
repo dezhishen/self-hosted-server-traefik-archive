3df75e15589309d21690c71e18c469812c0c433f
dezhiShen <1179551960@qq.com>
2022-03-30 23:00:28 +0800

    mod:代理多个端口

--- scripts/install-v2raya.sh
+++ scripts/install-v2raya.sh
@@ -17,6 +17,12 @@ docker run -d \
     --label 'traefik.http.routers.v2raya.priority=1000' \
     --label 'traefik.http.routers.v2raya.entrypoints=web' \
     --label "traefik.http.services.v2raya.loadbalancer.server.port=2017" \
+    --label 'traefik.http.routers.v2raya-proxy.rule=Host(`v2raya-proxy'.$domain'`)' \
+    --label "traefik.http.routers.v2raya-proxy.tls=false" \
+    --label "traefik.http.services.v2raya-proxy.loadbalancer.server.port=20171" \
+    --label 'traefik.http.routers.v2raya-proxy-rule.rule=Host(`v2raya-proxy-rule'.$domain'`)' \
+    --label "traefik.http.routers.v2raya-proxy-rule.tls=false" \
+    --label "traefik.http.services.v2raya-proxy-rule.loadbalancer.server.port=20172" \
     --label "traefik.enable=true" \
     -v $base_data_dir/v2raya:/etc/v2raya \
   mzz2017/v2raya
