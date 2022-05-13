7d0d722f04b4a6828d587efb51a0ff11ae096b81
dezhishen <1179551960@qq.com>
2022-05-13 20:07:37 +0800

    修改

--- scripts/install-jellyfin.sh
+++ scripts/install-jellyfin.sh
@@ -29,7 +29,7 @@ esac
 docker run -d \
 --restart=always \
 --name=jellyfin \
--m 128M --memory-swap=256M \
+-m 512M --memory-swap=1024M \
 --network=$docker_network_name \
 --network-alias=jellyfin \
 -e TZ="Asia/Shanghai" \
@@ -37,6 +37,14 @@ docker run -d \
 -e PUID=`id -u` -e PGID=`id -g` \
 -v $base_data_dir/jellyfin/config:/config \
 -v $base_data_dir/public/:/data \
+-v /opt/vc/lib:/opt/vc/lib \
+-v /dev/shm:/config/data/transcoding-temp/transcodes \
+--device /dev/dri:/dev/dri  \
+--device /dev/vchiq:/dev/vchiq  \
+--device /dev/video10:/dev/video10 \
+--device /dev/video11:/dev/video11 \
+--device /dev/video12:/dev/video12 \
+--device /dev/video13:/dev/video13 \
 --label 'traefik.http.routers.jellyfin.rule=Host(`jellyfin'.$domain'`)' \
 --label "traefik.http.routers.jellyfin.tls=true" \
 --label "traefik.http.routers.jellyfin.tls.certresolver=traefik" \
@@ -45,11 +53,9 @@ docker run -d \
 --label "traefik.enable=true" \
 lscr.io/linuxserver/jellyfin:$arch-latest
 
-
-`dirname $0`/create-docker-macvlan-network.sh
+#`dirname $0`/stop-container.sh qbittorrent
 
 docker_macvlan_network_name=$(`dirname $0`/get-args.sh docker_macvlan_network_name "macvlan的网络名")
-
 
 echo "加入到macvlan网络中..."
 docker network connect $docker_macvlan_network_name jellyfin --alias jellyfin-macvlan
